f3d05835a275cfb9dc1b933959b1d1fcb3ec004c
Aerdscheff <tarik@aerdscheff.lu>
2025-09-14 23:10:35 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+const SUPABASE_URL = "https://lofsckrqyrouymqyfatm.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxvZnNja3JxeXJvdXltcXlmYXRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyNDAyNTksImV4cCI6MjA3MTgxNjI1OX0.hUCEkHV8e7nz_BQgkvo-R2q6NC2Y5UdViV5LTV9l8A8"; //
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
