da382d75119adb4b104f211fdd28d12596449327
Aerdscheff <tarik@aerdscheff.lu>
2025-09-25 21:50:34 +0200

    Update admin.js

--- web/js/views/admin.js
+++ web/js/views/admin.js
@@ -1,8 +1,8 @@
 import { renderLayout } from '../layout.js';
 
 export async function render(app) {
-  let content = `
-    <h1 class="text-2xl font-bold text-[#E25C5C] mb-6">⚙️ Administration</h1>
+  const content = `
+    <h1 class="text-3xl font-exo2 text-[#E25C5C] mb-6">⚙️ Administration</h1>
     <p class="text-gray-700">Outils à venir : gestion des utilisateurs, exports, paramètres.</p>
   `;
   renderLayout(app, content);
